20c58163b4959b49f397f603017f02fd850442cd
Alexander Caicedo <a.caicedo.dev@gmail.com>
2026-05-10 11:07:06 -0500

    style(layout): adopt topbar header in default layout for asesor_tic

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -2,9 +2,25 @@
 <html lang="es">
 <?= $this->element('head') ?>
 <body>
-    <div class="overflow-auto scroll" style="height: 100dvh;">
+    <nav class="top-navbar app-topbar">
+        <div class="d-flex justify-content-between align-items-center px-3 w-100">
+            <img src="<?= $this->Url->image('logos/soporte-interno.svg') ?>">
+            <div class="nav-menu d-flex align-items-center gap-3 py-3">
+                <?= $this->Html->link('<i class="bi bi-ticket"></i> Tickets', ['prefix' => false, 'controller' => 'Tickets', 'action' => 'index'], ['escape' => false]) ?>
+                <?= $this->Html->link('<i class="bi bi-person"></i> Mi Perfil', ['prefix' => 'Admin', 'controller' => 'Settings', 'action' => 'editUser', $currentUser->id], ['escape' => false]) ?>
+            </div>
+            <div class="nav-user d-flex align-items-center gap-2">
+                <?= $this->Html->link('<i class="bi bi-box-arrow-right"></i> Salir', ['prefix' => false, 'controller' => 'Users', 'action' => 'logout'], ['class' => 'btn-logout', 'escape' => false]) ?>
+            </div>
+        </div>
+    </nav>
+
+    <div class="overflow-auto sidebar-scroll below-topbar-mh">
         <?= $this->Flash->render() ?>
-        <?= $this->fetch('content') ?>
+        <?= $this->element('loading_spinner') ?>
+        <div class="d-flex below-topbar-h">
+            <?= $this->fetch('content') ?>
+        </div>
     </div>
 </body>
 </html>
